5ca1e4eca24205bfa9c7da01840e2fe8ac16df6a
eansengchang <76442284+eansengchang@users.noreply.github.com>
2021-08-06 21:07:40 +0100

    Update spam.js

--- commands/commands/spam.js
+++ commands/commands/spam.js
@@ -3,6 +3,7 @@ const { inspect } = require('util');
 module.exports = {
     name: 'spam',
     description: 'specifically for spamming',
+    cooldown: 60,
     async execute(message, args) {
         if (message.guild.id !== '512578878305337354') return;
         let member = message.mentions.members.first();
